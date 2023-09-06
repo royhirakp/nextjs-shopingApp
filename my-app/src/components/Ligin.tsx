858c7b02d84ae4eea703671eedd3f879301a934c
royhirakp <royhirakp@gmail.com>
2023-09-06 13:17:15 +0530

    login page

--- my-app/src/components/Ligin.tsx
+++ my-app/src/components/Ligin.tsx
@@ -1,14 +1,11 @@
 "use client";
 import React from "react";
 
-import Image from "next/image";
 import {
   Avatar,
   Box,
   Button,
-  Checkbox,
   FormControl,
-  FormControlLabel,
   IconButton,
   InputAdornment,
   InputLabel,
@@ -21,6 +18,9 @@ import {
   Typography,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const UserBox = styled(Box)(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
@@ -35,9 +35,11 @@ const UserBox = styled(Box)(({ theme }) => ({
 const NewButton = styled(Button)(({ theme }) => ({
   textTransform: "none",
   color: "white",
+  marginTop: "1%",
+  borderRadius: "16px",
 
   [theme.breakpoints.up("xs")]: {
-    fontSize: "50%",
+    fontSize: "55%",
     width: "50px",
     "&.MuiButton-contained": {
       backgroundColor: "red",
@@ -87,37 +89,84 @@ const Ligin = () => {
         height: "100%",
       }}
     >
-      <Paper elevation={3} sx={{ padding: "2px" }}>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: {
+            xs: "2%",
+            sm: "3%",
+            md: "4%",
+          },
+          borderRadius: "15px",
+        }}
+      >
         <Stack alignItems="center" justifyContent="center">
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
           <h3>Login</h3>
         </Stack>
-        <TextField label="Emial" placeholder="Enter Email" fullWidth required />
-        <FormControl fullWidth variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+
+        <Stack justifyContent="center" alignItems="center">
+          <TextField
+            sx={{
+              marginBottom: "5px",
+            }}
+            label="Emial"
+            placeholder="Enter Email"
+            fullWidth
+            required
           />
-        </FormControl>
-        <NewButton type="submit" color="primary" variant="contained" fullWidth>
-          Sign in
-        </NewButton>
+          <FormControl fullWidth variant="outlined" sx={{}}>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <Typography>
+            <Link href="#" underline="hover">
+              {"Forget password ?"}
+            </Link>
+          </Typography>
+
+          <NewButton
+            type="submit"
+            color="primary"
+            variant="contained"
+            fullWidth
+          >
+            Sign in
+          </NewButton>
+        </Stack>
+
+        <Stack direction="row" spacing={1}>
+          <IconButton aria-label="delete">
+            <FacebookIcon />
+          </IconButton>
+          <IconButton aria-label="delete" disabled color="primary">
+            <FacebookIcon />
+          </IconButton>
+          <IconButton color="secondary" aria-label="add an alarm">
+            <GoogleIcon />
+          </IconButton>
+          <IconButton color="primary" aria-label="add to shopping cart">
+            <TwitterIcon />
+          </IconButton>
+        </Stack>
       </Paper>
     </Box>
   );
